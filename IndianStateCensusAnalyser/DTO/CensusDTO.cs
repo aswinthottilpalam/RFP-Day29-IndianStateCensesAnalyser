fbf38585e59b73bfaa20816c5a70b3058301882b
aswinthottilpalam <aswinp264@gmail.com>
2022-03-30 15:05:10 +0530

    Indian state Census ANalyser

---
+++ IndianStateCensusAnalyser/DTO/CensusDTO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser.POCO
+{
+    public class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public string state;
+        public int pin;
+        public string stateCode;
+        public long population;
+        public long area;
+        public long density;
+        public long housingUnits;
+        public double waterArea;
+        public double landArea;
+        public double populationDensity;
+        public double housingDensity;
+    }
+
+    public CensusDTO(StateCodeDAO stateCodeDao)
+    {
+        this.serialNumber = stateCodeDao.serialNumber;
+        this.stateName = stateCodeDao.stateName;
+        this.pin = stateCodeDao.pin;
+        this.stateCode = stateCodeDao.stateCode;
+    }
+
+    public CensusDTO(CensusDataDAO censusDataDao)
+    {
+        this.state = censusDataDao.state;
+        this.population = censusDataDao.population;
+        this.area = censusDataDao.area;
+        this.density = censusDataDao.density;
+    }
+
+    //public CensusDTO(UCCensusDAO usCensusDAO)
+    //{
+    //    this.stateCode = usCensusDAO.stateId;
+    //    this.stateName = usCensusDAO.stateName;
+    //    this.population = usCensusDAO.population;
+    //    this.housingUnits = usCensusDAO.housingUnits;
+    //    this.totalArea = usCensusDAO.totalArea;
+    //    this.waterArea = usCensusDAO.waterArea;
+    //    this.landArea = usCensusDAO.landArea;
+    //    this.populationDensity = usCensusDAO.populationDensity;
+    //    this.housingDensity = usCensusDAO.housingDensity;
+    //}
+
+}
---
+++ IndianStateCensusAnalyser/DTO/StateCodeDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser.POCO
+{
+    public class StateCodeDAO
+    {
+        public int serialNumber;
+        public string stateName;
+        public int pin;
+        public string stateCode;
+    }
+    public CensusDataDAO(string v1, string v2, string v3, string v4)
+    {
+        this.serialNumber = Convert.ToInt32(v1);
+        this.stateName = v2;
+        this.pin = Convert.ToInt32(v3);
+        this.stateCode = v4;
+    }
+}
